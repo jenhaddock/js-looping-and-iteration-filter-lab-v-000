21d227405098f9c629590c72d7345fe448c9f001
Jennifer Haddock <jentx123@hotmail.com>
2017-11-09 21:13:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file
 function findMatching(array, match){
   return array.filter(function(matchIt){
-    return array.toLowerCase() === match.toLowerCase();
+    return matchIt.toLowerCase() === match.toLowerCase();
   })
 }
